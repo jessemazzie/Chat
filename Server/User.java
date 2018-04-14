62d1f60e4b5e4a865746d3ebb9f45f2d39bbf56e
Jesse Mazzie <jessegalt@yahoo.com>
2018-04-14 01:10:29 -0400

    Copied over user and userlist classes.

---
+++ Server/User.java
@@ -0,0 +1,36 @@
+package Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Vector;
+
+public class User {
+    String username;
+    String password;
+    //CTC ctc;
+    Vector<String> friendsList;
+
+    User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    User(DataInputStream dis) throws IOException {
+        this.username = dis.readUTF();
+        this.password = dis.readUTF();
+    }
+
+    void send(String messageToBroadcast) {
+//        try {
+//            ctc.talker.send(messageToBroadcast);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    void store(DataOutputStream dos) throws IOException {
+        dos.writeUTF(username);
+        dos.writeUTF(password);
+    }
+}
---
+++ Server/UserList.java
@@ -0,0 +1,37 @@
+package Server;
+
+import java.io.*;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Hashtable;
+
+public class UserList extends HashMap<String, User> {
+
+    public UserList(){}
+
+    public UserList(DataInputStream dis) throws IOException {
+        int numUsers = dis.readInt();
+
+        for(int i = 0; i < numUsers; i++) {
+            User tempUser = new User(dis);
+            put(tempUser.username, tempUser);
+        }
+    }
+
+    public void addUser(User user) {
+        put(user.username, user);
+        try {
+            store();
+        } catch(IOException ioe) {}
+    }
+
+    void store() throws IOException {
+        File file = new File("users.xyz");
+        Enumeration<User> users = elements();
+        DataOutputStream dos = new DataOutputStream(new FileOutputStream((file)));
+        System.out.println("Number of elements" + size());
+        dos.writeInt(size());
+        while(users.hasMoreElements())
+            users.nextElement().store(dos);
+    }
+}
