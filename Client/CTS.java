ca4213b71969b284c59a0b464bebd740a9981655
Jesse Mazzie <jessegalt@yahoo.com>
2018-04-15 10:36:34 -0400

    Added CTS (Connection to Server) class.

---
+++ Client/CTS.java
@@ -0,0 +1,46 @@
+package Client;
+
+import Shared.Talker;
+
+import java.io.IOException;
+import java.net.Socket;
+
+///CTS = "Connection to server"
+public class CTS implements Runnable {
+    Client client;
+    Talker talker;
+    //MessageBox messageBox;
+
+    public CTS(Client client) throws IOException {
+
+        this.client = client;
+        talker = new Talker("127.0.0.1", 1337, "");
+
+        new Thread(this).start();
+    }
+
+    void send(String stringToSend) throws IOException {
+        talker.send(stringToSend);
+    }
+
+    @Override
+    public void run() {
+        String msg;
+        try {
+            while (true) {
+                msg = talker.receive().trim();
+                System.out.println("Message in CTC: " + msg);
+                if(msg.equals("LOGGED_IN")) {
+//                    client.setVisible(false);
+//                    messageBox = new MessageBox(this);
+                } else if(msg.startsWith("BROADCAST")) {
+                    System.out.println(msg);
+//                    messageBox.messageList.addElement(msg.substring(9));
+//                    messageBox.repaint();
+                }
+            }
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
